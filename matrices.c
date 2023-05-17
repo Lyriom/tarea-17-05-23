c3c6d264eb30f3cddbde6f7380467688e9cca6c1
josue riera <josueriera23@hotmail.com>
2023-05-17 15:18:50 -0500

    primera version

---
+++ matrices.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int filas, columnas;
+    printf("Ingrese el numero de filas de la matriz:\n");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de la matriz: \n");
+    scanf("%d", &columnas);
+
+    // Crear la matriz
+    int matriz1[filas][columnas];
+
+    // Inicializar la matriz con ceros
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz1[i][j] = 0;
+        }
+    }
